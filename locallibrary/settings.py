7706ef07635fb364763c1a71ce4e24035478550c
unknown <lair60@yahoo.es>
2021-01-29 17:45:05 +0000

    Added files for cron and work

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 # importing os module  
 import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'locallibrary.settings'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
