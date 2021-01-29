8ef58f266842e358c88462b71e362d3660d424de
unknown <lair60@yahoo.es>
2021-01-29 14:46:10 +0000

    Added files for cron and work

--- locallibrary/utils.py
+++ locallibrary/utils.py
@@ -5,6 +5,6 @@ from .models import TemporalLink
 def removeLinks():
     datetime_object = datetime.datetime.now()
     ten_minute = datetime.timedelta(minutes=10)
-	time_filter = datetime_object - ten_minute
+    time_filter = datetime_object - ten_minute
     TemporalLink.objects.filter(created_at__lte = time_filter).delete()
     print('Removed')
